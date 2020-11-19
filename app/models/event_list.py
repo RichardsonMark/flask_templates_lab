93e6937a7a5811d77d69371adfd6a11ae24e9154
Mark Richardson <mark_richardson@sky.com>
2020-11-19 15:04:12 +0000

    Setting up html file structure and adding list of events

--- app/models/event_list.py
+++ app/models/event_list.py
@@ -2,8 +2,8 @@ from app.models.event import *
 
 
 
-event1 = Event("Christmas market", "24 Dec", "12", "Town Hall", "A fun time for the socially distanced small crowd",  True)
-event2 = Event("Hogmanay Hootenany", "31 Dec", "24", "Town Hall", "Celebrate the end of 2020", False)
+event1 = Event("Christmas market", "12-24 Dec", "12 tickets", "Town Hall", "A fun time for the socially distanced small crowd",  True)
+event2 = Event("Hogmanay Hootenany", "31 Dec", "24 tickets", "Town Hall", "Celebrate the end of 2020", False)
 events = [event1, event2]
 
 
